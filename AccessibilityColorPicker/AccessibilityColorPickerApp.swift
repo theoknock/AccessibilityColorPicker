6f7099a554964439f0e25fe8e4a7658978f91314
James Alan Bush <jamesalanbush1972@gmail.com>
2024-04-25 03:16:02 -0400

    Initial Commit

---
+++ AccessibilityColorPicker/AccessibilityColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityColorPickerApp.swift
+//  AccessibilityColorPicker
+//
+//  Created by Xcode Developer on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
